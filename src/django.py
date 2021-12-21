f25071e10756691a8a300e82353273ee35a765f9
thismatters <paul@thismatters.net>
2021-12-21 15:48:06 -0600

    should be plurals here too

--- src/django.py
+++ src/django.py
@@ -6,13 +6,13 @@ from kinds import DjangoKind
 # https://github.com/kubernetes-client/python/blob/master/kubernetes/README.md
 
 
-@kopf.on.update("thismatters.github", "v1alpha", "django")
-@kopf.on.create("thismatters.github", "v1alpha", "django")
+@kopf.on.update("thismatters.github", "v1alpha", "djangos")
+@kopf.on.create("thismatters.github", "v1alpha", "djangos")
 async def created(**kwargs):
     return await DjangoKind().update_or_create(**kwargs)
 
 
-# @kopf.on.timer("thismatters.net", "v1alpha", "django", interval=30)
+# @kopf.on.timer("thismatters.net", "v1alpha", "djangos", interval=30)
 # def scale_deployment(**kwargs):
 #     DjangoKind().scale_deployment(**kwargs)
 #     DjangoKind().scale_deployment(deployment="worker", **kwargs)
