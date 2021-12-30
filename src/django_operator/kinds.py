ee9d5b9833c008b2225b86d3a2d01ddf0492c9b8
thismatters <paul@thismatters.net>
2021-12-30 09:45:51 -0600

    stupid bugs

--- src/django_operator/kinds.py
+++ src/django_operator/kinds.py
@@ -202,7 +202,8 @@ class DjangoKind:
             enrichments=enrichments,
             existing=green_name,
             template=template,
-            raw=True ** kwargs,
+            raw=True,
+            **kwargs,
         )
         ret = {kind: {purpose: green_obj.metadata.name}}
 
--- src/django_operator/tests/test_kinds.py
+++ src/django_operator/tests/test_kinds.py
@@ -1,7 +1,8 @@
 from unittest import TestCase
+from unittest.mock import patch
 
 from django_operator.kinds import DjangoKind
-
+from django_operator.services import PodService
 
 class MockLogger:
     def info(self, *args, **kwargs):
@@ -29,7 +30,8 @@ class DjangoKindTestCase(TestCase):
             },
             namespace="test",
         )
-        former, existing = django_kind._deployment_names(
+        former, existing = django_kind._resource_names(
+            kind="deployment",
             purpose="app",
         )
         self.assertEqual(former, "app-6-9-420")
@@ -52,8 +54,39 @@ class DjangoKindTestCase(TestCase):
             },
             namespace="test",
         )
-        former, existing = django_kind._deployment_names(
+        former, existing = django_kind._resource_names(
+            kind="deployment",
             purpose="app",
         )
         self.assertEqual(former, None)
         self.assertEqual(existing, "app-6-9-420")
+
+    @patch.object(PodService, "ensure")
+    def test_ensure_kwargs(self, p_ensure):
+        status = {
+            "created": {"deployment": {"app": "app-6-9-420", "beat": "beat-6-8-4"}}
+        }
+        django_kind = DjangoKind(
+            logger=MockLogger(),
+            status=status,
+            patch={},
+            body={"this": "body"},
+            spec={
+                "host": "test.somewhere.com",
+                "clusterIssuer": "letsencrypt",
+                "version": "6.9.420",
+                "image": "testimage",
+            },
+            namespace="test",
+        )
+
+        django_kind.clean_manage_commands(pod_name="poopypod")
+        p_ensure.assert_called_once_with(
+            namespace="test",
+            template="pod_migrations.yaml",
+            parent={"this": "body"},
+            purpose="migrations",
+            delete=True,
+            existing="poopypod",
+            **django_kind.base_kwargs,
+        )
